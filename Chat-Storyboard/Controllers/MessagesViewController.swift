bbe54667786763f7c8335c00f9bb31f246677c64
Dante Solorio <contact@deipi.com>
2016-10-11 12:06:13 -0500

    Ep 4, add messages table

---
+++ Chat-Storyboard/Controllers/MessagesViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MessagesViewController.swift
+//  Chat-Storyboard
+//
+//  Created by Dante Solorio on 10/11/16.
+//  Copyright © 2016 iOS School Mexico. All rights reserved.
+//
+
+import UIKit
+
+class MessagesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var messagesTableView: UITableView!
+    
+    let messageCellId = "messageCellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table delegates
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = messagesTableView.dequeueReusableCell(withIdentifier: messageCellId, for: indexPath) as! MessageTableViewCell
+        cell.messageLabel.text = "Message \(indexPath.row)"
+        if indexPath.row % 2 == 0{
+            cell.messageLabel.textAlignment = .right
+        }
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
