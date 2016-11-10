4666704fbb8c324609965569a205296d22a95d9a
Dante Solorio <contact@deipi.com>
2016-11-09 18:20:52 -0600

    Add message model, and show messages in table message view

--- Chat-Storyboard/Controllers/MessagesViewController.swift
+++ Chat-Storyboard/Controllers/MessagesViewController.swift
@@ -14,15 +14,38 @@ class MessagesViewController: UIViewController, UITableViewDelegate, UITableView
     
     let messageCellId = "messageCellId"
     
+    var messages = [Message]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        getExampleData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - General functions
+    fileprivate func getExampleData(){
+        let message1 = Message()
+        message1.messageText = "Hi"
+        message1.messageUserId = 1
+        
+        let message2 = Message()
+        message2.messageText = "Hello"
+        message2.messageUserId = 2
+        
+        let message3 = Message()
+        message3.messageText = "How are you?"
+        message3.messageUserId = 1
+        
+        let message4 = Message()
+        message4.messageText = "Fine, how about you?"
+        message4.messageUserId = 2
+        
+        messages += [message1, message2, message3, message4]
     }
     
     // MARK: - Table delegates
@@ -31,14 +54,21 @@ class MessagesViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return messages.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = messagesTableView.dequeueReusableCell(withIdentifier: messageCellId, for: indexPath) as! MessageTableViewCell
-        cell.messageLabel.text = "Message \(indexPath.row)"
-        if indexPath.row % 2 == 0{
+        
+        let message = messages[indexPath.row]
+        
+        // Message text
+        cell.messageLabel.text = message.messageText
+        // Message user
+        if message.messageUserId == 1{
             cell.messageLabel.textAlignment = .right
+            cell.backgroundColor = .blue
+            cell.messageLabel.textColor = .white
         }
         return cell
     }
